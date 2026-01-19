804e9272a2569bdfbfa070cc19ceabba72e0c74c
Arthur <arthur.pinto.neves@gmail.com>
2026-01-19 17:13:12 -0300

    posicao da 'lampada' e funcao andar

--- app.js
+++ app.js
@@ -25,11 +25,19 @@ let robo = document.getElementById('robo');
 start.appendChild(robo); //coloca o robo na posicao inicial 
 
 
-let lamp = document.getElementById('lamp');
-let lampPos = document.getElementById(coord(lampada.x, lampada.y));
+let lamp = document.getElementById('lamp'); //pega a lampada
+let lampPos = document.getElementById(coord(lampada.x, lampada.y)); //pega a posicao da lampada
 lampPos.appendChild(lamp); //coloca a lampada na posicao determinada
 
 
-function andar() {
-
+function andar(){
+    if(player.direction === 0){
+        player.y -= 1;
+    } else if(player.direction === 1){
+        player.x += 1;
+    } else if(player.direction === 2){
+        player.y += 1;
+    } else if(player.direction === 3){
+        player.x -= 1;
+    }
 }
